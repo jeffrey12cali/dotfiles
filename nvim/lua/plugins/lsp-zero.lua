80f89edbeca5bd4d15cce99482ba571a4d0867af
Jeffrey García <jeffrey12cali@outlook.com>
2025-05-23 22:04:46 +0200

    Waybar changes and updating avante

--- nvim/lua/plugins/lsp-zero.lua
+++ nvim/lua/plugins/lsp-zero.lua
@@ -26,23 +26,6 @@ return {
             }
         })
 
-        require("mason-lspconfig").setup_handlers({
-            -- Will be called for each installed server that doesn't have
-            -- a dedicated handler.
-            --
-            function(server_name) -- default handler (optional)
-                -- https://github.com/neovim/nvim-lspconfig/pull/3232
-                if server_name == "tsserver" then
-                    server_name = "ts_ls"
-                end
-                local capabilities = require("cmp_nvim_lsp").default_capabilities()
-                require("lspconfig")[server_name].setup({
-
-                    capabilities = capabilities,
-                })
-            end,
-        })
-
         local cmp = require('cmp')
         local cmp_select = {behavior = cmp.SelectBehavior.Select}
         local cmp_mappings = lsp.defaults.cmp_mappings({
